d6893bb2cc995f93b05ce5f32175da6b284d9319
Odrian <adrian46tver@bk.ru>
2025-08-04 23:28:48 +0300

    add compiling tests

---
+++ tests/test_compiling.rs
@@ -0,0 +1,88 @@
+use std::default::Default;
+use std::process::Command;
+use std::sync::atomic;
+use programming_language::parser::Config;
+
+fn get_exit_code(text: &str) -> i32 {
+    static COUNTER: atomic::AtomicU32 = atomic::AtomicU32::new(0);
+    let id = COUNTER.fetch_add(1, atomic::Ordering::Relaxed);
+    let name = format!("test{id}");
+    let path = format!("./{name}");
+
+    let config = Config {
+        output: name,
+        ..Default::default()
+    };
+    let chars = text.chars().collect::<Vec<_>>();
+    programming_language::parser::parse(&chars, config).unwrap();
+    
+    let code = Command::new(&path).status();
+    let is_deleted = Command::new("rm").arg(&path).status().unwrap().success();
+    assert!(is_deleted, "{}", format!("can't delete {path}"));
+
+    code.unwrap().code().unwrap()
+}
+
+#[test]
+fn test_simplest() {
+    assert_eq!(5, get_exit_code("\
+main :: () {
+    return 5
+}
+"));
+
+    assert_eq!(3, get_exit_code("\
+main :: () {
+    return 1 + 2
+}
+"));
+
+    assert_eq!(7, get_exit_code("\
+main :: () {
+    a := 7
+    return a
+}
+"));
+
+    assert_eq!(9, get_exit_code("\
+main :: () {
+    a := 7
+    a = 9
+    return a
+}
+"));
+
+    assert_eq!(12, get_exit_code("\
+foo :: () {
+    return 12
+}
+
+main :: () {
+    return foo()
+}
+"));
+}
+
+#[test]
+fn test_fibonachi() {
+    let program = "\
+fibonachi :: (n) {
+    f0 := 0
+    f1 := 1
+    while n {
+        f0 = f0 + f1
+        temp := f0
+        f0 = f1
+        f1 = temp
+        n = n - 1
+    }
+    return f0
+}
+
+main :: () {
+    return fibonachi(10)
+}
+";
+    let result = get_exit_code(program);
+    assert_eq!(result, 55);
+}
